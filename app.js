0c2c0fedcb978319c3710656e196e51901f8b71a
Donguyen297 <nguyendxgch210885@fpt.edu.vn>
2023-10-23 23:46:08 +0700

    ok

--- app.js
+++ app.js
@@ -65,6 +65,6 @@ app.use(function (err, req, res, next) {
 });
 
 // cấu hình port của server để deploy lên cloud
-app.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT || 35166);
 
 module.exports = app;
